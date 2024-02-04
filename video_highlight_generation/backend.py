9d1a3a152c1d65048cb6527f5e1ba7c7c26ef237
Sean Javiya <sjaviya@nvidia.com>
2024-02-03 16:48:41 -0800

    wip sean

--- video_highlight_generation/backend.py
+++ video_highlight_generation/backend.py
@@ -1,9 +1,10 @@
 # main.py
+from pathlib import Path
 
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import os
-from typing import List, Tuple
+from typing import List, Tuple, Annotated
 import imageio
 import numpy as np
 
@@ -36,16 +37,23 @@ def extract_segments(input_file: str, segments: List[Tuple[str, str]]) -> str:
     return output_file
 
 @app.post("/process_video")
-async def process_video(input_file: UploadFile = File(...)) -> dict:
+# async def process_video(input_file: UploadFile = File(...)) -> dict:
+# async def process_video(data: dict) -> dict:
+async def process_video(input_file: Annotated[UploadFile, File()]) -> dict:
     try:
+        # input_file = data.get('file')
         input_file_path = f"uploads/{input_file.filename}"
+        base_path = Path(__file__).parent
+        input_file_path = Path.joinpath(base_path, 'test_video.mp4')
+        # df = pd.read_excel(file_path)
         with open(input_file_path, "wb") as f:
             f.write(input_file.file.read())
 
-        time_ranges = [("60", "203"), ("480", "627"), ("1929", "2589")]  # in seconds
-        output_file = extract_segments(input_file_path, time_ranges)
+        time_ranges = [("60", "100"), ("140", "220"), ("260", "300")]  # in seconds
+        output_file = extract_segments(str(input_file_path), time_ranges)
 
         return {"status": "success", "message": "Video processed successfully", "output_file": output_file}
 
     except Exception as e:
+        print(e)
         raise HTTPException(status_code=500, detail=str(e))
--- video_highlight_generation/frontend.py
+++ video_highlight_generation/frontend.py
@@ -1,37 +1,45 @@
-# streamlit_app.py
-
 import streamlit as st
 import requests
-import cv2
-import numpy as np
 from io import BytesIO
 
-st.title("Video Cropper App")
+st.title("Video Upload and Display App")
 
-uploaded_file = st.file_uploader("Upload a video", type=["mp4"])
+# File upload widget
+uploaded_file = st.file_uploader("Upload a video file", type=["mp4", "avi", "mov"])
 
 if uploaded_file:
     st.video(uploaded_file)
 
     if st.button("Process Video"):
-        files = {"file": uploaded_file}
-        response = requests.post("http://localhost:8000/process_video", files=files)
+        # Display a progress bar for the video upload
+        progress_bar = st.progress(0)
+        uploaded_percentage = 0
+
+        # Create a stream buffer to upload the video in chunks
+        # video_stream = BytesIO()
+
+        with uploaded_file:
+            for chunk in uploaded_file.__iter__():
+                # video_stream.write(chunk)
+                uploaded_percentage += len(chunk) / uploaded_file.size
+                progress_bar.progress(uploaded_percentage)
+
+        # bytes_data = uploaded_file.read()
+        # uploaded_file.seek(0)
+        # files = {"file": uploaded_file}
+        with open(uploaded_file.name, "rb") as f:
+            f.seek(0)
+            file_bytes = f.read()
+            response = requests.post("http://localhost:8000/process_video", files={'vid.mp4': file_bytes})
+        # response = requests.post("http://localhost:8000/process_video", files={'vid.mp4': open(uploaded_file, 'r')})
+        # Reset the stream position before sending the request
+        # video_stream.seek(0)
+
+        # Send the video to the backend for processing
+        # files = {"file": ("video_file", video_stream, "video/mp4")}
 
         if response.status_code == 200:
             st.success("Video processed successfully!")
-
-            # Display the processed video using OpenCV
-            output_file = response.json()["output_file"]
-            processed_video = cv2.VideoCapture(output_file)
-            
-            while True:
-                ret, frame = processed_video.read()
-                if not ret:
-                    break
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                st.image(frame, channels="RGB", use_column_width=True)
-
-            processed_video.release()
-
+            st.video(response.content)
         else:
             st.error("Error processing video. Please try again.")
