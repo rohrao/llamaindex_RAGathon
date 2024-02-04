9fd7319e1171793db71323de790ee44c3d7909f6
Sean Javiya <sjaviya@nvidia.com>
2024-02-03 18:43:42 -0800

    working :rocket:

--- video_highlight_generation/backend.py
+++ video_highlight_generation/backend.py
@@ -8,6 +8,8 @@ from moviepy.editor import concatenate_videoclips
 import os
 from typing import List, Tuple, Any
 
+from starlette.responses import StreamingResponse, FileResponse
+
 app = FastAPI()
 
 origins = ["http://localhost", "http://localhost:3000"]  # Add the frontend URL here
@@ -30,13 +32,14 @@ def extract_segments(input_file: Any, segments: List[Tuple[str, str]]) -> str:
     final_clip = concatenate_videoclips(clips)
 
     output_file = "output.mp4"
-    final_clip.write_videofile(output_file, codec="libx264", audio_codec="aac")
+    video = final_clip.write_videofile(output_file, codec="libx264", audio_codec="aac")
 
     return output_file
 
 
 @app.post("/process_video")
-async def process_video(input_file: UploadFile = File(...)) -> dict:
+async def process_video(input_file: UploadFile = File(...)) -> FileResponse:
+# async def process_video(input_data: dict, input_file: UploadFile = File(...)) -> FileResponse:
     try:
         input_file_path = f"uploads/{input_file.filename}"
         base_path = Path(__file__).parent
@@ -46,9 +49,12 @@ async def process_video(input_file: UploadFile = File(...)) -> dict:
             f.write(input_file.file.read())
 
         time_ranges = [("00:01:00", "00:01:23"), ("00:02:00", "00:02:08"), ("00:02:34", "00:02:43")]
-        output_file = extract_segments(input_file_path, time_ranges)
-
-        return {"status": "success", "message": "Video processed successfully", "output_file": output_file}
-
+        video_file = extract_segments(input_file_path, time_ranges)
+        # with open(video_file, "rb") as f:
+        #     video_bytes = f.read()
+        #     files = {"input_file": ("video.mp4", video_file, "video/mp4")}
+        # return {"status": "success", "message": "Video processed successfully", "output_file": output_file}
+        # return StreamingResponse(video_file, media_type="video/mp4")
+        return FileResponse(video_file, media_type='video/mp4')
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
